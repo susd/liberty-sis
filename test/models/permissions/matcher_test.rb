fa42da962ab1266dd31fc4235b4ce455ae3c8d46
Eric Hayes <eric@erichayes.net>
2015-08-18 11:09:37 -0700

    WIP perms

--- test/models/permissions/matcher_test.rb
+++ test/models/permissions/matcher_test.rb
@@ -7,14 +7,20 @@ class Permissions::MatcherTest < ActiveSupport::TestCase
     @matcher = Permissions::Matcher.new(@user)
 
     @edit = Permissions::Ability.new(:edit)
+
+    @own = Permissions::Level.new(:own)
   end
 
   test "Finds resource ability" do
     assert_equal @edit, @matcher.resource_ability(:classrooms)
   end
 
-  test "Finds target ability" do
-    assert_equal @edit, @matcher.target_ability(classrooms(:ashleys_class))
+  test "Level match" do
+    assert_equal @own, @matcher.scope_match?(@own, classrooms(:ashley_doe))
   end
+
+  # test "Finds target ability" do
+  #   assert_equal @edit, @matcher.target_ability(classrooms(:ashleys_class))
+  # end
 
 end
