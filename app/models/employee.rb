a17df972f1cad25ca7a602df34a00b8b31a16f47
Eric Hayes <eric@erichayes.net>
2015-09-03 13:52:38 -0700

    Annotate

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,23 +2,24 @@
 #
 # Table name: employees
 #
-#  id             :integer          not null, primary key
-#  type           :string
-#  first_name     :string
-#  last_name      :string
-#  sex            :string
-#  email          :string
-#  birthdate      :date
-#  hired_on       :date
-#  years_edu      :integer          default(0), not null
-#  years_district :integer          default(0), not null
-#  title          :string
-#  status         :integer          default(0), not null
-#  legacy_id      :integer
-#  user_id        :integer
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  import_details :jsonb            default({}), not null
+#  id              :integer          not null, primary key
+#  type            :string
+#  first_name      :string
+#  last_name       :string
+#  sex             :string
+#  email           :string
+#  birthdate       :date
+#  hired_on        :date
+#  years_edu       :integer          default(0), not null
+#  years_district  :integer          default(0), not null
+#  title           :string
+#  status          :integer          default(0), not null
+#  legacy_id       :integer
+#  user_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  import_details  :jsonb            default({}), not null
+#  primary_site_id :integer
 #
 
 class Employee < ActiveRecord::Base
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: settings
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  data       :jsonb            default({}), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Setting < ActiveRecord::Base
   def self.[](name)
     find_by(name: name)
