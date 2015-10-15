9b657c9f803d503ed2ea56dbeba04ea744b3fb77
Eric Hayes <eric@erichayes.net>
2015-10-15 14:43:45 -0700

    WIP dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    if current_user.employee && current_user.employee.type == 'Teacher'
+      redirect_to classrooms_path
+    end
     authorize_signed_in_user!
   end
 end
