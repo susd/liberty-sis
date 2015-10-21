a7b580df38eedf81cd49f913df2c13fe53b299c2
Eric Hayes <eric@erichayes.net>
2015-10-21 09:05:56 -0700

    Production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address  => "smtp.mandrillapp.com",
+    :port     => 587,
+    :domain   => "saugususd.org",
+    :user_name => Rails.application.secrets.mandrill_user,
+    :password => Rails.application.secrets.mandrill_pass
+  }
+
+  config.action_mailer.default_url_options = { host: 'liberty.saugususd.org' }
+
+  config.middleware.use ExceptionNotification::Rack,
+    ignore_exceptions: ExceptionNotifier.ignored_exceptions, # + [AnnoyingError]
+    email: {
+      email_prefix: "[Liberty-Error] ",
+      sender_address: %{"Liberty" <liberty-sis@saugususd.org>},
+      exception_recipients: %w{ehayes@saugususd.org}
+    }
 end
