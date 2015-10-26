fcf52497cc3c41b329d76445516d53a53a57e973
Eric Hayes <eric@erichayes.net>
2015-10-26 16:20:13 -0700

    WIP importing json cards

--- app/importers/report_cards/json_importer.rb
+++ app/importers/report_cards/json_importer.rb
@@ -12,7 +12,7 @@ module ReportCards
     def extract_data
       @builder = {}
       @builder[:form] = @form
-      @builder[:student] = Student.find_by(["import_details -> 'import_id' = ?", @data['student_id']])
+      @builder[:student] = Student.find_by(["import_details -> 'import_id' = ?", @data['student_id'].to_json])
       @builder[:year] = ReportCard::GradingPeriod.school_year_for(DateTime.parse(@data['created_at']))
       @builder[:data] = {
         comments: extract_comments,
@@ -23,18 +23,23 @@ module ReportCards
 
     def extract_comments
       hsh = {}
-      data['data']['comments'].each do |p, cmts|
-        hsh[p] = cmts.map{|e| @form.comments.find_by(english: e).id }
+      data['data']['comments'].each_with_index do |cmts, idx|
+        hsh[idx + 1] = {comment_ids: cmts[(idx + 1).to_s].map{|e| @form.comments.find_by(english: e).id }}
       end
       hsh
     end
 
     def extract_subjects
-
+      hsh = {subjects: {}}
+      data['data']['subjects'].each do |sub_hsh|
+        subject = @form.subjects.find_by(slug: sub_hsh.first[0])
+        hsh[:subjects][subject.id] = sub_hsh.first[1]
+      end
+      hsh
     end
 
     def extract_attendance
-      
+
     end
 
   end
