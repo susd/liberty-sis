f9b1cbd245da57c9443320dc0850d2d22bc23fdf
Eric Hayes <eric@erichayes.net>
2015-10-29 10:23:07 -0700

    More accurate legend for each card and better code org.

--- app/presenters/report_card/legend_presenter.rb
+++ app/presenters/report_card/legend_presenter.rb
@@ -1,9 +1,23 @@
 class ReportCard::LegendPresenter < BasePresenter
   presents :report_card
 
-  def render_legend
-    # render report_card.form.renderer
+  def score_scale
+    if %w{tk kinder primary}.include? report_card.form.renderer
+      type = 'positional'
+    else
+      type = 'value'
+    end
+
+    tpl.render partial: "report_cards/legends/#{type}_score"
   end
 
+  def effort_scale
+    if %w{tk kinder}.include? report_card.form.renderer
+      type = 'easy'
+    else
+      type = report_card.form.renderer
+    end
 
+    tpl.render partial: "report_cards/legends/#{type}_effort"
+  end
 end
