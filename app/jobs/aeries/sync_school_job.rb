169362b214a08e641d76effa515781e760d92b68
Eric Hayes <eric@erichayes.net>
2015-10-29 16:22:59 -0700

    Schedule school sync

--- app/jobs/aeries/sync_school_job.rb
+++ app/jobs/aeries/sync_school_job.rb
@@ -2,9 +2,9 @@ module Aeries
   class SyncSchoolJob < ActiveJob::Base
     queue_as :pdf
 
-    # after_perform do
-    #   SyncSchoolJob.set(wait: 1.hour).perform_later
-    # end
+    after_perform do
+      SyncSchoolJob.set(wait: 2.hours).perform_later
+    end
 
     def perform(school_code)
       Aeries::SchoolImporter.new(school_code).import_recent!
