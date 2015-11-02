0de53196833df554c5fee26ec7de18f381c16585
Eric Hayes <eric@erichayes.net>
2015-11-02 10:45:53 -0800

    Fix comment editing

--- app/controllers/report_cards/comments_controller.rb
+++ app/controllers/report_cards/comments_controller.rb
@@ -3,13 +3,27 @@ module ReportCards
     before_action :authenticate_user!
     before_action :authorize_admin!
     before_action :set_form
+    helper_method :submit_path_for
 
     def index
       @comments = comments_scope.all
     end
 
+    def new
+      @comment = @form.comments.new
+    end
+
     def edit
       @comment = comments_scope.find(params[:id])
+    end
+
+    def create
+      @comment = @form.comments.new(comment_params)
+      if @comment.save
+        redirect_to report_cards_form_comments_path(@form), notice: 'Comment saved.'
+      else
+        render :new
+      end
     end
 
     def update
@@ -29,11 +43,19 @@ module ReportCards
     end
 
     def comment_params
-      params.require(:comment).permit(:english, :spanish, :report_card_comment_group_id)
+      params.require(:report_card_comment).permit(:english, :spanish, :report_card_comment_group_id)
     end
 
     def set_form
       @form = ReportCard::Form.find(params[:form_id])
+    end
+
+    def submit_path_for(form, comment)
+      if comment.new_record?
+        report_cards_form_comments_path(form)
+      else
+        report_cards_form_comment_path(form, comment)
+      end
     end
 
   end
