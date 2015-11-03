53f1cd22a0dca288b677e702f5df8fc3f28933fa
Eric Hayes <eric@erichayes.net>
2015-11-03 10:55:15 -0800

    Useful dashboard

---
+++ app/presenters/dashboard_presenter.rb
@@ -0,0 +1,18 @@
+class DashboardPresenter
+
+  def user_count
+    User.count
+  end
+
+  def teacher_count
+    Teacher.count
+  end
+
+  def rc_year_count
+    ReportCard.where(year: ReportCard::GradingPeriod.school_year).count
+  end
+
+  def rc_period_count
+    ReportCard.where(created_at: ReportCard::GradingPeriod.current_year_range).count
+  end
+end
