b3b27f4b21433525a097a8b30d44fb27d8066fff
Eric Hayes <eric@erichayes.net>
2015-11-13 10:26:31 -0800

    Allow home lang change

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,7 @@ class StudentsController < ApplicationController
 
   def search
     authorize_general(:view, :all, :students)
-    
+
     query = params[:term] || params[:query]
     @students = Student.includes(:homeroom, :site).admin_search(query).limit(20)
 
@@ -15,9 +15,34 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    load_student
+    load_languages
+    authorize_to(:edit, @student)
+  end
+
+  def update
+    load_student
+    authorize_to(:edit, @student)
+    if @student.update(student_params)
+      redirect_to @student, notice: 'Student updated'
+    else
+      load_languages
+      render :edit
+    end
+  end
+
   private
 
   def load_student
     @student = Student.includes(:classrooms).find(params[:id])
+  end
+
+  def load_languages
+    @langs = Language.where(name: %w{English Spanish}).pluck(:name, :id)
+  end
+
+  def student_params
+    params.require(:student).permit(:home_lang_id)
   end
 end
