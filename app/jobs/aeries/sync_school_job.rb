502f628a7f539dee1d88af67fa1ffaf3593d69e5
Eric Hayes <eric@erichayes.net>
2016-01-13 15:32:52 -0800

    Do not reschedule full school import

--- app/jobs/aeries/sync_school_job.rb
+++ app/jobs/aeries/sync_school_job.rb
@@ -2,10 +2,6 @@ module Aeries
   class SyncSchoolJob < ActiveJob::Base
     queue_as :sync
 
-    after_perform do |job|
-      SyncSchoolJob.set(wait: 2.hours).perform_later(*job.arguments)
-    end
-
     def perform(school_code)
       Aeries::SchoolImporter.new(school_code).import!
     end
