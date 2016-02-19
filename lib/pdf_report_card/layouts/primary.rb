3e37c951d37b770561ddd57dc8054e1730e38e9e
Eric Hayes <eric@erichayes.net>
2016-02-19 14:14:54 -0800

    Add unsatisfactory effort

--- lib/pdf_report_card/layouts/primary.rb
+++ lib/pdf_report_card/layouts/primary.rb
@@ -1,8 +1,18 @@
 module PdfReportCard
   module Layouts
-    
-    class Primary < PdfReportCard::Layouts::Base      
+
+    class Primary < PdfReportCard::Layouts::Base
+
+      def legend_items
+        [
+          "O - Outstanding",
+          "G - Good", "S - Satisfactory",
+          "N - Needs Improvement",
+          "U - Unsatisfactory",
+          "✓ - Quality of Work"
+        ]
+      end
     end
-    
+
   end
 end
