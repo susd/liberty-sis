7ef8037c976aabbfaf21a6867e32c86fb52af264
Eric Hayes <eric@erichayes.net>
2016-03-02 13:02:19 -0800

    WIP vertical column headings

--- test/lib/pdf_report/components/vertical_header_test.rb
+++ test/lib/pdf_report/components/vertical_header_test.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
 class PdfReport::Components::VerticalHeaderTest < PdfTestCase
-
+  def setup
+    @doc = PdfReport::Document.new
+    @rect = PdfReport::Geometry::Rect.new({
+      width: ((@doc.canvas_width * 0.65) - 110),
+      height: 112,
+      y: 682,
+      x: 110
+      })
+    # @header = 
+  end
 end
