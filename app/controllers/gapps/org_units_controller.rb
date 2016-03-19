ec1a92aca03446549555460d970019bd11f4b21c
Eric Hayes <eric@erichayes.net>
2016-03-18 21:58:14 -0700

    WIP manage org units

--- app/controllers/gapps/org_units_controller.rb
+++ app/controllers/gapps/org_units_controller.rb
@@ -2,6 +2,12 @@ module Gapps
   class OrgUnitsController < ApplicationController
 
     def index
+      @org_units = Gapps::OrgUnit.roots.includes(:children)
+      authorize_general(:manage, :all, :gapps_org_units)
+    end
+
+    def new
+      @org_unit = Gapps::OrgUnit.new
       authorize_general(:manage, :all, :gapps_org_units)
     end
   end
